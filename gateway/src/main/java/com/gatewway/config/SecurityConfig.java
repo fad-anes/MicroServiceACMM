d5601740d1e402c464877c3721bed712993033c0
fad-anes <anes.fad@esprit.tn>
2023-12-11 22:23:44 +0100

    Dockerfile

--- gateway/src/main/java/com/gatewway/config/SecurityConfig.java
+++ gateway/src/main/java/com/gatewway/config/SecurityConfig.java
@@ -6,7 +6,9 @@ import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:4200")
 @Configuration
 @EnableWebFluxSecurity
 @RequiredArgsConstructor
@@ -17,7 +19,11 @@ public class SecurityConfig {
         return serverHttpSecurity.csrf(ServerHttpSecurity.CsrfSpec::disable)
                 .authorizeExchange(exchange -> exchange.pathMatchers("/eureka/**")
                         .permitAll()
-                        .pathMatchers("/user/one/**").hasRole("admin")
+                        .pathMatchers("/user/login/**").permitAll()
+                        .pathMatchers("/user/create/**").hasAnyRole("admin","user")
+                        .pathMatchers("/user/**").hasRole("admin")
+                        .pathMatchers("/avis/all/**").permitAll()
+                        .pathMatchers("/avis/**").hasAnyRole("admin","user")
                         .anyExchange().authenticated()
                 ).oauth2ResourceServer((oauth) -> oauth
                         .jwt(jwtSpec ->jwtSpec .jwtAuthenticationConverter(jwtAuthConverter)))
